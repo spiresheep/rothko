9d5ec1d1fcb8e5652082713026d0d7d26ef1e91c
Sofiya Polyanska <sofiya@spiretrading.com>
2022-03-17 19:47:39 -0400

    Factor out Cell and Parse

--- rothko.py
+++ rothko.py
@@ -1,46 +1,7 @@
-# hello_world.py
-import json
+from rothko.parse import parse
 import PySimpleGUI as sg
 
-#export later
-class Cell:
-  def __init__(self, top, left, width, w_color, height, h_color, name):
-    self.left = left
-    self.top = top
-    self.width = width
-    self.height = height
-    self.name = name
-    #uhhhh
-    self.w_color = w_color
-    self.h_color = h_color
-  def get_right(self):
-    end = self.left + self.width
-    return end
-
-  def get_bottom(self):
-    end = self.top + self.height
-    return end
-
-  def print(self):
-    print(self.top, self.left, self.width, self.w_color, self.height, self.h_color, self.name)
-
-def parse():
-  print('Start parse');
-  file = open('demo_file.txt', 'r')
-  local = json.loads(file.read())
-  cells = []
-  for cell in local['cells']:
-    splitH = cell["horizontal"].split(" ")
-    splitV = cell["vertical"].split(" ")
-    name = ""
-    if "name" in cell:
-      name = cell["name"]
-    newCell = Cell(cell["top"], cell["left"], int(splitH[0]), splitH[1], int(splitV[0]), splitV[1], name)
-    cells.append(newCell)
-  print("Done parse")
-  return cells
-
-def drawCheck(cells):
+def get_min_dimensions(cells):
   max_width = 0
   max_height = 0
   for cell in cells:
@@ -52,18 +13,28 @@ def drawCheck(cells):
       max_height = bottom
   return [max_width, max_height]
 
-def draw_border(canvas, outline_width, fill, left, top, right, bottom):
-  return canvas.TKCanvas.create_rectangle(left, top, right, bottom, fill=fill, width=outline_width, outline='white')
+def draw_rectangle(canvas, outline_width, fill, left, top, right, bottom):
+  result = canvas.TKCanvas.create_rectangle(
+    left,
+    top,
+    right,
+    bottom,
+    fill=fill,
+    width=outline_width,
+    outline='white'
+  )
+  return result
 
 if __name__ == "__main__":
-  cells_to_draw = parse()
-  min_size = drawCheck(cells_to_draw)
+  cells_to_draw = parse('demo_file_2.txt')
+  min_size = get_min_dimensions(cells_to_draw)
   width = min_size[0]
   height = min_size[1]
-  layout = [[sg.Canvas(size=(width, height), background_color='black', key= 'canvas')]]
+  layout = [[
+    sg.Canvas(size=(width, height), background_color='black', key= 'canvas')
+  ]]
   window = sg.Window('Canvas test', layout, finalize=True)
   canvas = window['canvas']
-  cir = canvas.TKCanvas.create_oval(50, 50, 100, 100)
   for cell in cells_to_draw:
     left = cell.left
     right = cell.get_right()
@@ -71,35 +42,73 @@ if __name__ == "__main__":
     bottom = cell.get_bottom()
     width_color = cell.w_color
     height_color = cell.h_color
+    yellow = '#ffbb00'
     #draw a solid color rectangle
     if cell.w_color == cell.h_color:
       if cell.w_color == 'yellow':
-        canvas.TKCanvas.create_rectangle(left, top, right, bottom, fill='#ffbb00', outline='red')
+        canvas.TKCanvas.create_rectangle(
+          left,
+          top,
+          right,
+          bottom,
+          fill=yellow,
+          outline=''
+        )
       else:
-        canvas.TKCanvas.create_rectangle(left, top, right, bottom, fill=cell.w_color, outline='green')
+        canvas.TKCanvas.create_rectangle(
+          left,
+          top,
+          right,
+          bottom,
+          fill=cell.w_color,
+          outline=''
+        )
     #draw a lined rectangle
     else:
-      canvas.TKCanvas.create_rectangle(left, top, right, bottom, fill='lightgrey', outline='pink')
+      canvas.TKCanvas.create_rectangle(
+        left, top, right, bottom, fill='lightgrey'
+      )
       outline_width = 1
       border_size = 2
-      #TODO better lines
       #draw blue lines 
       if width_color == 'blue':
-        draw_border(canvas, outline_width, 'blue', left, top, right - outline_width, top + border_size + outline_width) #very good
-        draw_border(canvas, outline_width, 'blue', left, bottom - border_size - (2 * outline_width), right - outline_width, bottom- outline_width) #crap
+        draw_rectangle(
+          canvas, outline_width, 'blue',
+          left, top, right - outline_width, top + border_size + outline_width
+        )
+        draw_rectangle(
+          canvas, outline_width, 'blue',
+          left,
+          bottom - border_size - (2 * outline_width),
+          right - outline_width,
+          bottom - outline_width
+        )
       if height_color == 'blue':
-        draw_border(canvas, outline_width, 'blue', left, top, left + border_size + outline_width, bottom - outline_width)  #crap
-        draw_border(canvas, outline_width, 'blue', right - border_size - (2 * outline_width), top, right - outline_width, bottom - outline_width)  #crap
-      # #draw yellow lines #ffbb00
+        draw_rectangle(
+          canvas,
+          outline_width,
+          'blue',
+          left,
+          top,
+          left + border_size + outline_width, bottom - outline_width
+        )
+        draw_rectangle(canvas, outline_width, 'blue', right - border_size - (2 * outline_width), top, right - outline_width, bottom - outline_width)  #crap
+      # #draw yellow lines
       if width_color == 'yellow':
-        draw_border(canvas, outline_width, '#ffbb00', left, top, right - outline_width, top + border_size + outline_width) #very good
-        draw_border(canvas, outline_width, '#ffbb00', left, bottom - border_size - (2 * outline_width), right - outline_width, bottom- outline_width) #crap
+        draw_rectangle(canvas, outline_width, '#ffbb00', left, top, right - outline_width, top + border_size + outline_width) #very good
+        draw_rectangle(canvas, outline_width, '#ffbb00', left, bottom - border_size - (2 * outline_width), right - outline_width, bottom- outline_width) #crap
       if height_color == 'yellow':
-        draw_border(canvas, outline_width, '#ffbb00', left, top, left + border_size + outline_width, bottom - outline_width)  #crap
-        draw_border(canvas, outline_width, '#ffbb00', right - border_size - (2 * outline_width), top, right - outline_width, bottom - outline_width)  #crap
+        draw_rectangle(canvas, outline_width, '#ffbb00', left, top, left + border_size + outline_width, bottom - outline_width)  #crap
+        draw_rectangle(canvas, outline_width, '#ffbb00', right - border_size - (2 * outline_width), top, right - outline_width, bottom - outline_width)  #crap
     #write text
     if cell.name != '':
       middle_height = (top + bottom) / 2
       middle_width = (left + right) / 2
-      canvas.TKCanvas.create_text(middle_width, middle_height, text=cell.name, width=(right-left), fill='black')
+      canvas.TKCanvas.create_text(
+        middle_width,
+        middle_height,
+        text=cell.name,
+        width=(right-left),
+        fill='black'
+      )
   window.read()
---
+++ rothko/parse.py
@@ -0,0 +1,26 @@
+from rothko.cell import Cell
+import json
+
+def parse(file_name):
+  print('Start parse');
+  file = open(file_name, 'r')
+  local = json.loads(file.read())
+  cells = []
+  for cell in local['cells']:
+    splitH = cell["horizontal"].split(" ")
+    splitV = cell["vertical"].split(" ")
+    name = ""
+    if "name" in cell:
+      name = cell["name"]
+    newCell = Cell(
+      cell["top"],
+      cell["left"],
+      int(splitH[0]),
+      splitH[1],
+      int(splitV[0]),
+      splitV[1],
+      name
+    )
+    cells.append(newCell)
+  print("Done parse")
+  return cells
