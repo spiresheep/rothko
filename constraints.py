2838229eda3ae3503634618c61b1972f449ce771
Sofiya Polyanska <sofiya@spiretrading.com>
2022-04-26 18:57:37 -0400

    Proof of concept for two adaptable and one fixed works

--- constraints.py
+++ constraints.py
@@ -232,12 +232,116 @@ def _proof_of_concept_constraints_from_cells_adaptable(): #!WIP
   print('symbols_list', symbols_list)
   print('Maximum Solution', solution)
 
+#CURRENTLY WORKING ON
 
+def _proof_of_concept_constraints_from_cells_MULTIPLE_adaptable(): #!WIP
+  demo_cells = [
+    Cell(0, 0, 100, 'fixed', 100, 'fixed', 'B'),
+    Cell(0, 100, 100, 'adaptable', 100, 'adaptable', 'A'),
+    Cell(0, 200, 100, 'adaptable', 100, 'adaptable', 'C')
+  ]
+  layout = Layout(demo_cells)
+
+
+  fixed_constraints = []
+  names_of_adaptable_cells = []
+  current_node = layout.graph.get_horizontal_source()
+
+  # Get constraints from fixed cells and adaptable cells
+  while (current_node != None):
+    #print('current_node', current_node.cell.get_name())
+    if (current_node.cell.w_policy == 'fixed') & (current_node.cell.name != 'WEST'):
+      fixed_constraints.append(f'{current_node.cell.name}_width = {current_node.cell.width}')
+    elif current_node.cell.w_policy == 'adaptable':
+      names_of_adaptable_cells.append(current_node.cell.get_name())
+    if (current_node.get_east() != []):
+      current_node = current_node.get_east()[0]
+    else:
+      current_node = None
+  # Generate the adaaptable cell constraint
+  adaptable_constrains = []
+  if(len(names_of_adaptable_cells) == 1):
+    cell_name = names_of_adaptable_cells[0]
+    adaptable_constrains.append(f'{cell_name}_width = {cell_name}_width ')
+  elif(len(names_of_adaptable_cells) > 1):
+    first_cell_name = names_of_adaptable_cells[0]
+    for other_cell in names_of_adaptable_cells:
+      adaptable_constrains.append(f'{other_cell}_width = {first_cell_name}_width')
+    
+  print('ADAPTABLE CONSTRAINTS', adaptable_constrains)
+  #IF MIN_WIDTH OR MAX_WIDHTS ARE IN THE FORMULA... add plox
+  base_constaints = []
+  for constraint in fixed_constraints + adaptable_constrains:
+    if 'MAX_WIDTH' in constraint:
+      base_constaints = [f'MAX_WIDTH = {MAX_WIDTH}']
+      break
+  core_constraints = parse_constraints(base_constaints + fixed_constraints + adaptable_constrains)
+  # Generate the canvas constraint
+  rhs = ''
+  for constraint in core_constraints:
+    if((str(constraint.get_symbol()) != 'MAX_WIDTH')  & (str(constraint.get_symbol()) != 'MIN_WIDTH')):
+      if len(rhs) != 0:
+        rhs = rhs + '+' + str(constraint.get_symbol())
+      else:
+        rhs = str(constraint.get_symbol())
+  width_constraint = f'canvas_width = {rhs}'
+  core_constraints.append(Constraint(width_constraint))
+  # Minimized adaptable cells
+  minimizing_constraint = []
+  if(len(names_of_adaptable_cells) > 1):
+    cell_name = names_of_adaptable_cells[0]
+    minimizing_constraint.append(f'{cell_name}_width = 0')
+  #NOW SOLVE <3
+  symbols_list = []
+  f_list = []
+  for constraint in core_constraints + parse_constraints(minimizing_constraint):
+    symbols_list.append(constraint.get_symbol())
+    f_list.append(constraint.get_equation())
+  solution = sympy.solve(f_list, symbols_list)
+  print('flist', f_list)
+  print('symbols_list', symbols_list)
+  print('Minimum Solution', solution)
+  # Maximize adaptable cells
+  maximzing_constraint = []
+  if(len(names_of_adaptable_cells) >= 1):
+    cell_name = names_of_adaptable_cells[0]
+    print ('CELLLLL NAME', cell_name)
+    current_node = layout.graph.get_horizontal_source()
+    # Get constraints from fixed cells and adaptable cells
+    rhs = ''
+    while (current_node != None):
+      if (current_node.cell.get_name() != cell_name) & (current_node.cell.get_name() != 'WEST'):
+        name = current_node.cell.name + '_width'
+        if len(rhs) != 0:
+          rhs = rhs + '-' + name
+        else:
+          rhs = name
+      if (current_node.get_east() != []):
+        current_node = current_node.get_east()[0]
+      else:
+        current_node = None
+    maximzing_constraint.append(f'{cell_name}_width = {MAX_WIDTH} - {rhs}')
+    print('MAXIMIZING CONSTRAINT', maximzing_constraint)
+  #NOW SOLVE <3
+  symbols_list = []
+  f_list = []
+  for constraint in core_constraints + parse_constraints(maximzing_constraint):
+    symbols_list.append(constraint.get_symbol())
+    f_list.append(constraint.get_equation())
+  #remove symbol duplicates
+  solution = sympy.solve(f_list, symbols_list)
+  print('flist', f_list)
+  print('symbols_list', symbols_list)
+  print('Maximum Solution', solution)
+
+#END OF CURRENT WORK OF PROCESS
 if __name__ == "__main__":
   print('~~Start Test~~')
   #_proof_of_concept_parse_and_solve_single_fixed()
   #_proof_of_concept_parse_and_solve_single_adaptable()
   #_proof_of_concept_constraints_from_cells_fixed()
   print('~~Adaptable Cell Demo~~')
-  _proof_of_concept_constraints_from_cells_adaptable()
+  #_proof_of_concept_constraints_from_cells_adaptable()
+
+  _proof_of_concept_constraints_from_cells_MULTIPLE_adaptable()
   print('~~End Test~~')
